6bd11961e6b33a9a6d181a8b05e7471153242abb
Felix Horger <felix.horger@kcl.ac.uk>
2022-03-21 16:40:45 +0100

    Improved structuring of multiple DICOMData objects

--- src/DICOMTools.jl
+++ src/DICOMTools.jl
@@ -6,46 +6,120 @@ module DICOMTools
 	export dcm2array
 	export load_dcms, load_dcms!
 	export merge_multiseries
+	export DICOMDict
+	export DICOMSeries
 
 	using DICOM
 
 	# Sorry for the bad commenting/documenting ... when I find the time I'll add it
 
+	# Define a types to store DICOMs found in a directory structure
+	struct DICOMSeries
+		paths::Vector{String} # Individual dicom files
+		dcms::Vector{DICOM.DICOMData} # The read in dicom structures
+		pos::Vector{Int64} # Position in the files where reading stopped (see enum WhatToRead)
+	end
+	const DICOMDict = Dict{String, DICOMSeries} # SeriesInstanceUID => DICOMSeries
+
+	# This enum can be used to define up to which group to read
+	@enum WhatToRead read_all read_uid read_voxelsize skip_pixels
+	# Convert above enum to a DICOM group
+	function get_max_group(what::WhatToRead)::UInt16
+		# Get DICOM.dcm_parse's max_group argument to stop reading early
+		max_group = (what == read_all) ? 0xffff :
+					(what == read_uid) ? 0x0020 :
+					(what == read_voxelsize) ? 0x0028 : 0x7FDF # last option must be skip_pixels
+		return max_group
+	end
+
+
+
+	# Load Dicoms recursively
+	function load_dcms(directory::String, what::WhatToRead=read_all)::DICOMDict
+
+		max_group = get_max_group(what)
+
+		dcmdict = DICOMDict()
+		for (root, _, files) in walkdir(directory)
+			for filename in files
+				fullpath = joinpath(root, filename)
+				DICOM.isdicom(fullpath) || continue
+				local dcm, pos
+				open(fullpath, "r") do file
+					dcm = dcm_parse(file; max_group)
+					if eof(file)
+						pos = -1
+					else
+						pos = position(file) - sizeof(UInt16) # Group tag was read in, revert position
+					end
+				end
+				uid = dcm[tag"SeriesInstanceUID"]
+				if haskey(dcmdict, uid)
+					push!(dcmdict[uid].paths, fullpath)
+					push!(dcmdict[uid].dcms, dcm)
+					push!(dcmdict[uid].pos, pos)
+				else
+					dcmdict[uid] = DICOMSeries([fullpath], [dcm], [pos])
+				end
+			end
+		end
+		return dcmdict
+	end
+
+	# Read tags retrospectively
+	function load_dcms!(dcmdict::DICOMDict, what::WhatToRead=read_all)::DICOMDict
+		max_group = get_max_group(what)
+		for series in values(dcmdict)
+			load_dcms!(series, max_group)
+		end
+		return dcmdict
+	end
+	function load_dcms!(series::DICOMSeries, what::WhatToRead=read_all)::DICOMSeries
+		max_group = get_max_group(what)
+		load_dcms!(series::DICOMSeries, max_group)
+		return series
+	end
+	function load_dcms!(series::DICOMSeries, max_group::UInt16)::DICOMSeries
+		for (path, dcm, pos) in zip(series.paths, series.dcms, series.pos)
+			pos == -1 && continue
+			open(path, "r") do file
+				seek(file, pos)
+				DICOM.read_body!(file, dcm; max_group)
+			end
+		end
+		return series
+	end
+
+
+	function filter(f::Function, dcmdict::DICOMDict)
+		Base.filter(series -> f(series.second.dcms), dcmdict)
+	end
+	function filter!(f::Function, dcmdict::DICOMDict)
+		Base.filter!(series -> f(series.second.dcms), dcmdict)
+	end
 
 
 	function get_tag(
-		multiseries::AbstractVector{<: AbstractVector{DICOM.DICOMData}},
-		tag::Tuple{UInt16,UInt16};
-		outtype::Type = Nothing
-	)
-		# Get type
-		if outtype <: Nothing
-			outtype = typeof(multiseries[1][1][tag])
-		end
-
-		# Get values
-		return outtype[ dcms[1][tag] for dcms in multiseries ]
+		multiseries::AbstractVector{<: DICOMSeries},
+		tag::Tuple{UInt16,UInt16},
+		outtype::Val{T} = Val(typeof(multiseries[1].dcms[1][tag]))
+	) where T
+		T[ series.dcms[1][tag] for series in multiseries ]
 	end
 	function get_tags(
-		multiseries::AbstractVector{<: AbstractVector{DICOM.DICOMData}},
-		tags::NTuple{N, Tuple{UInt16,UInt16}};
-		outtypes::NTuple{N, Type} = ntuple(i -> Nothing, N)
-	) where N
-		# Get type
-		for i = 1:N
-			if outtypes[i] <: Nothing
-				outtypes[i] = typeof(multiseries[1][1][tags[i]])
-			end
-		end
-
-		# Get values
-		return Tuple{outtypes...}[ dcms[1][tag] for tag in tags for dcms in multiseries ]
+		multiseries::AbstractVector{<: DICOMSeries},
+		tags::NTuple{N, Tuple{UInt16,UInt16}},
+		outtypes::Val{T} = Val(ntuple(i -> typeof(multiseries[1].dcms[1][tags[i]]), N))
+	) where {N, T}
+		T[ series.dcms[1][tag] for tag in tags for series in multiseries ]
 	end
 
 
-	function get_transformation_matrix(dcms::AbstractVector{<: DICOM.DICOMData})
+	function get_transformation_matrix(series::DICOMSeries)::Matrix{Float64}
 		# This breaks if InstanceNumber tag does not equal slice number
 		# Gaps between slices are handled correclty if volume is rectangular
+
+		dcms = series.dcms
 
 		# Get origin of first and second slice
 		# Translation of first slice is origin of the volume
@@ -86,11 +160,13 @@ module DICOMTools
 
 
 	function dcm2array(
-		dcms::AbstractVector{<: DICOM.DICOMData};
-		outtype::Type = Nothing
-	)::Array{<: Real, 3}
+		series::DICOMSeries,
+		outtype::Val{T} = Val(Float64)
+	)::Array{T, 3} where T
 		# Note: This breaks if InstanceNumber tag does not equal slice number
-		
+
+		dcms = series.dcms
+
 		# Get size
 		reference = dcms[1]
 		array_size = (
@@ -99,13 +175,8 @@ module DICOMTools
 			maximum( get(dcm, tag"InstanceNumber", 1) for dcm in dcms )
 		)
 
-		# Get type
-		if outtype <: Nothing
-			outtype = eltype(reference[tag"PixelData"])
-		end
-
 		# Fill array
-		voxels = Array{outtype, 3}(undef, array_size...)
+		voxels = Array{T, 3}(undef, array_size...)
 		for dcm in dcms
 			data = dcm[tag"PixelData"]
 			voxels[:, :, dcm[tag"InstanceNumber"]] = data
@@ -116,90 +187,7 @@ module DICOMTools
 
 
 
-	# Define a type to store DICOMs found in a directory structure
-	const DICOMDict = Dict{
-		String,
-		NamedTuple{
-			(
-				:paths,	# Individual dicom files
-				:dcms,	# The read dicom structures
-				:pos	# Position in the files where reading stopped (see enum WHAT_TO_READ)
-			),
-			Tuple{Vector{String}, Vector{DICOM.DICOMData}, Vector{Int64}}
-		}
-	}
 
-
-
-	# This enum can be used to define up to which group to read
-	@enum WHAT_TO_READ read_all read_uid read_voxelsize skip_pixels
-	# Convert above enum to a DICOM group
-	function what_to_read_2_max_group(what::WHAT_TO_READ)::UInt16
-		# Get DICOM.dcm_parse's max_group argument to stop reading early
-		max_group = (what == read_all) ? 0xffff :
-					(what == read_uid) ? 0x0020 :
-					(what == read_voxelsize) ? 0x0028 : 0x7FDF # last option must be skip_pixels
-		return max_group
-	end
-
-
-
-	# Load Dicoms recursively
-	function load_dcms(directory::String, what::WHAT_TO_READ=read_all)::DICOMDict
-
-		max_group = what_to_read_2_max_group(what)
-
-		dcmdict = DICOMDict()
-		for (root, _, files) in walkdir(directory)
-			for filename in files
-				fullpath = joinpath(root, filename)
-				DICOM.isdicom(fullpath) || continue
-				local dcm, pos
-				open(fullpath, "r") do file
-					dcm = dcm_parse(file; max_group)
-					if eof(file)
-						pos = -1
-					else
-						pos = position(file) - sizeof(UInt16) # Group tag was read in, revert position
-					end
-				end
-				uid = dcm[tag"SeriesInstanceUID"]
-				if haskey(dcmdict, uid)
-					push!(dcmdict[uid].paths, fullpath)
-					push!(dcmdict[uid].dcms, dcm)
-					push!(dcmdict[uid].pos, pos)
-				else
-					dcmdict[uid] = (paths=[fullpath], dcms=[dcm], pos=[pos])
-				end
-			end
-		end
-		return dcmdict
-	end
-
-
-
-	# Read tags retrospectively
-	function load_dcms!(dcmdict::DICOMDict, what::WHAT_TO_READ=read_all)::DICOMDict
-		
-		max_group = what_to_read_2_max_group(what)
-
-		for dcms in values(dcmdict)
-			for (path, dcm, pos) in zip(dcms.paths, dcms.dcms, dcms.pos)
-				pos == -1 && continue
-				open(path, "r") do file
-					seek(file, pos)
-					DICOM.read_body!(file, dcm; max_group)
-				end
-			end
-		end
-
-		return dcmdict
-	end
-
-
-	function filter(f::Function, dcmdict::DICOMDict)
-		Base.filter(series -> f(series.second.dcms), dcmdict)
-	end
 
 
 	"""
@@ -210,9 +198,9 @@ module DICOMTools
 
 	"""
 	function merge_multiseries(
-		multiseries::AbstractVector{<: AbstractVector{<: DICOM.DICOMData}};
-		data_type::Type = Nothing
-	)::Array
+		multiseries::AbstractVector{<: DICOMSeries},
+		data_type::Val{T} = Val(eltype(multiseries[1].dcms[1]))
+	)::Array where T
 		# TODO: Would expect to return Array{..., 4} because dcm2array gives 3D volume.
 		# However what happens with timeseries in 3D volumes?
 
@@ -223,11 +211,8 @@ module DICOMTools
 		# Rearrange all volumes into a single array, first axis iterates over parameters,
 		# this makes access to signal(parameter) easier.
 		# Create empty array from the size of the first element in tojoin
-		if data_type <: Nothing
-			data_type = eltype(tojoin[1])
-		end
 		arrays = Array{
-			data_type,
+			T,
 			1 + ndims(tojoin[1]) # parameter (1) and spatial dimensions
 		}(
 			undef,
